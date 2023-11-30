8a10a3dfd2071d6b220e68cf021fdb594b2e247e
David Wallace <david.wallace@tu-darmstadt.de>
2023-11-30 13:31:58 +0100

    chore: add read_json file util

--- rdmo_plugins_somef/imports/utils.py
+++ rdmo_plugins_somef/imports/utils.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 import tomli as toml
-
+import json
 
 def load_config(file_name):
     toml_file = Path(__file__).parent / file_name
@@ -14,4 +14,13 @@ def load_config(file_name):
             "\nThe {} file is not a valid TOML file.\n\t{}".format(toml_file, exc)
         ) from exc
     
-# json.loads(jsonfile.read_text()).dump('01.scrape.pretty.json', sort_keys=True, indent=4)
+def read_json_file(json_file):
+    try:
+        with open(Path(json_file)) as f:
+            data = json.loads(f.read())
+        return data
+    except (json.decoder.JSONDecodeError, UnicodeDecodeError):
+        return None
+    except FileNotFoundError as exc:
+        print(f"File {json_file} not found")
+        return None
